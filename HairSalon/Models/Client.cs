29ca22821f17f9e1c17bf69ef79683fc47013cb4
Ashe Urban <ashe@goldentongue.com>
2022-07-29 14:11:33 -0700

    ADD Additional properties to Client class.

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -5,6 +5,8 @@ namespace HairSalon.Models
     public int ClientId { get; set; }
     public string Name { get; set; }
     public string ContactInfo { get; set; }
+    public string Notes { get; set; }
+    public string DateOfLastApp { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
   }
